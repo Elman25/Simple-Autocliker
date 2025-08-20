3c659fb8e94d230096b8ff7c8b5572168083c380
Elman <121354588+Elman25@users.noreply.github.com>
2025-08-20 17:19:32 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,10 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
+// main.cpp
+#include <QApplication>
+#include "autoclicker.h"
 
-int main(int argc, char *argv[])
-{
-    QGuiApplication app(argc, argv);
-
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/Autoclicker/Main.qml"));
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreated,
-        &app,
-        [url](QObject *obj, const QUrl &objUrl) {
-            if (!obj && url == objUrl)
-                QCoreApplication::exit(-1);
-        },
-        Qt::QueuedConnection);
-    engine.load(url);
-
-    return app.exec();
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    AutoClicker w;
+    w.show();
+    return a.exec();
 }
